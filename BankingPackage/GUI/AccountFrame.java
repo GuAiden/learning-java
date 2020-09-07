2538bac27c9169f4b63908ac35f34779221af2dd
Aiden Gu <aiden.gu01@gmail.com>
2020-09-08 01:45:23 +1000

    Still working on GUI

--- BankingPackage/GUI/AccountFrame.java
+++ BankingPackage/GUI/AccountFrame.java
@@ -9,15 +9,16 @@ public class AccountFrame extends JFrame implements ActionListener  {
 
     private Container container;
     private JPanel jLeft = new JPanel();
-    private JPanel jRight = new JPanel();
-    private JPanel jCenter = new JPanel();
-    private JPanel jTop = new JPanel();
-    private JPanel subJTop = new JPanel();
-    private JPanel jBottom = new JPanel();
+    private JPanel home = new JPanel();
+    private JPanel deposit = new JPanel();
+    private JPanel withdraw = new JPanel();
+    private JPanel buttonPanel = new JPanel();
     private JLabel headerLabel;
     private JLabel balance; 
-    private JButton deposit = new JButton("Deposit");
-    private JButton withdraw = new JButton("Withdraw");
+    private JLabel accountActions = new JLabel("Account Actions");
+    private JButton homeButton = new JButton("Home");
+    private JButton depositButton = new JButton("Deposit");
+    private JButton withdrawButton = new JButton("Withdraw");
 
     private static final long serialVersionUID = 1L;
 
@@ -25,56 +26,75 @@ public class AccountFrame extends JFrame implements ActionListener  {
         setFrame(account);
         setLayoutManager();
         colorPanels();
-        setComponenetPreferredSizes();
-        addComponents();
+        setMenuBar();
+        // setComponenetPreferredSizes();
+        addButtonsToPanel();
+        setAlignment();
+        addPanels();
     }
 
     public void setFrame(Account account) {
         this.container = getContentPane();
-        this.headerLabel = new JLabel(account.getName() + " " + "Account");
+        this.headerLabel = new JLabel(account.getName() + " " + "Bank Account");
         this.balance = new JLabel(Integer.toString(account.getBalance())); 
         this.setTitle("Account Manager");
-        this.setBounds(10, 10, 600, 600);
+        this.setBounds(0, 0, 900, 600);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setResizable(false);
+        this.setResizable(true);
+        // this.pack();
         this.setVisible(true);
     }
     public void setLayoutManager() {
-        BorderLayout layout = new BorderLayout(0, 0);
-        // jTop.setLayout(layout);
+        BorderLayout layout = new BorderLayout();
+        jLeft.setLayout(new GridBagLayout());
+        home.setLayout(new CardLayout());
+        buttonPanel.setLayout(new GridLayout(3, 1));
         this.setLayout(layout);
     }
 
-    public void setComponenetPreferredSizes() {
-        jTop.setPreferredSize(new Dimension(600, 100));
-        jCenter.setPreferredSize(new Dimension(500, 600));
-        jLeft.setPreferredSize(new Dimension(100, 800)); 
-        jRight.setPreferredSize(new Dimension(150, 600));
-        jBottom.setPreferredSize(new Dimension(600, 100));
-        subJTop.setPreferredSize(new Dimension(100, 100));
-        headerLabel.setPreferredSize(new Dimension(100, 100));
-        balance.setPreferredSize(new Dimension(50, 50));
-    }
-    public void colorPanels() {
-        Color modernBlue = new Color(81, 98, 247);
-        Color modernGrey = new Color(107, 109, 127);
-        jTop.setBackground(modernBlue);
-        subJTop.setBackground(modernGrey);
-        jLeft.setBackground(modernGrey);
-        jRight.setBackground(modernBlue);
-        jCenter.setBackground(modernBlue);
-        jBottom.setBackground(modernBlue);
-    }
-    public void addComponents() {
-        container.add(jTop, BorderLayout.NORTH);
-        container.add(jBottom, BorderLayout.SOUTH);
-        container.add(jCenter, BorderLayout.CENTER);
-        container.add(jLeft, BorderLayout.WEST);
-        container.add(jRight, BorderLayout.EAST);
+    public GridBagConstraints setMenuBar() {
+        GridBagConstraints c = new GridBagConstraints();
+        c.fill = GridBagConstraints.NONE;
+        c.gridx = 0;
+        c.gridy = 0;
+        c.insets = new Insets(50, 10, 50, 10);
+        jLeft.add(accountActions, c);
 
-        jTop.add(subJTop, BorderLayout.WEST);
-        // jTop.add(headerLabel);
-        jCenter.add(balance, BorderLayout.CENTER);
+        c.fill = GridBagConstraints.VERTICAL;
+        c.gridx = 0;
+        c.gridy = 1;
+        c.weightx = 0.5;
+        c.weighty = 0.5;
+        c.insets = new Insets(20, 20, 20, 20);
+        jLeft.add(buttonPanel, c);
+        return c;
+    }
+    public void addButtonsToPanel() {
+        buttonPanel.add(homeButton);
+        buttonPanel.add(depositButton);
+        buttonPanel.add(withdrawButton);
+    }
+    public void setAlignment() {
+        headerLabel.setHorizontalAlignment(JLabel.CENTER);
+        headerLabel.setBorder(BorderFactory.createLineBorder(new Color(65, 79, 209)));
+    }
+    public void setComponenetPreferredSizes() {
+        home.setPreferredSize(new Dimension(600, 600));
+        jLeft.setPreferredSize(new Dimension(200, 600)); 
+    }
+    public void colorPanels() {;
+        Color modernBlack = new Color(43, 45, 47);
+        jLeft.setBackground(Color.BLACK);
+        home.setBackground(modernBlack);
+        accountActions.setForeground(Color.WHITE); 
+        headerLabel.setForeground(Color.WHITE);
+        balance.setForeground(Color.WHITE);
+
+
+    }
+    public void addPanels() {
+        container.add(home, BorderLayout.CENTER);
+        container.add(jLeft, BorderLayout.WEST);
     }
 
     @Override
